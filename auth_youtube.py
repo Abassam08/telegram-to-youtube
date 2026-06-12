3b01de710619d453fab7b9b6db507ef38997c1b6
Abassam08 <abassam912@gmail.com>
2026-06-12 00:05:32 -0400

    Fix YouTube auth scope

--- auth_youtube.py
+++ auth_youtube.py
@@ -3,7 +3,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import config
 
-SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+SCOPES = ["https://www.googleapis.com/auth/youtube"]
 flow   = InstalledAppFlow.from_client_secrets_file(config.YOUTUBE_CLIENT_SECRETS, SCOPES)
 creds  = flow.run_local_server(port=8082, open_browser=False)
 
